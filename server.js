a0006f1339d61991c8fd00b54237ec56787018b4
Ben Clark <hello@clarkben.com>
2023-02-01 16:18:38 +0000

    Uses process.env.port for cloud deployment

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const fs = require('fs');
 const routes = require('./routes/routes.js');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app, fs);
 
-const server = app.listen(3001, () => {
+const server = app.listen(PORT, () => {
     console.log('listening on port %s...', server.address().port);
 });
